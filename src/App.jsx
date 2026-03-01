f61c55186a0f9051aa139b07399e6886cf54e700
= <zehabfaisal2003@gmail.com>
2026-03-01 12:38:10 +0500

    Phase2 Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
-import Home from './Pages/Home';
-import Navbar from './Components/Navbar'
 import { Routes, Route } from 'react-router-dom';
-import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import Jobs from './Pages/Jobs';
+import Application from './Pages/Application';
+import Dashboard from './Pages/Dashboard';
+import JobDetailsPage from './Pages/JobsDetailsPage';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup/>} />
+        <Route path='/jobs' element={<Jobs />} />
+        <Route path='/jobs/:id' element={<JobDetailsPage />} />
+        <Route path='/dashboard' element={< Dashboard />} />
+        <Route path='/applications' element={<Application />} />
       </Routes>
       <Footer />
     </div>
